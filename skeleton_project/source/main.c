1aa3e0b490313627ecc478e29bc3546f32e3cb72
Edvard Sire <mail@edvardsire.com>
2024-02-13 23:36:40 +0100

    first work

--- skeleton_project/source/main.c
+++ skeleton_project/source/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <stdbool.h>
 #include "hardware.h"
 
 static void clear_all_order_lights(){
@@ -18,68 +19,56 @@ static void clear_all_order_lights(){
     }
 }
 
-
-int main(){
-    int error = hardware_init();
-    if(error != 0){
-        fprintf(stderr, "Unable to initialize hardware\n");
-        exit(1);
-    }
-    
-    printf("=== Example Program ===\n");
-    printf("Press the stop button on the elevator panel to exit\n");
-
-    hardware_command_movement(HARDWARE_MOVEMENT_UP);
-
-    while(1){
-        if(hardware_read_stop_signal()){
-            hardware_command_movement(HARDWARE_MOVEMENT_STOP);
-            break;
-        }
-
-        /* Code block that makes the elevator go up when it reach the botton*/
-        if(hardware_read_floor_sensor(0)){
-            hardware_command_movement(HARDWARE_MOVEMENT_UP);
-        }
-
-        /* Code block that makes the elevator go down when it reach the top floor*/
-        if(hardware_read_floor_sensor(HARDWARE_NUMBER_OF_FLOORS - 1)){
-            hardware_command_movement(HARDWARE_MOVEMENT_DOWN);
-        }
-
+static void lights() {
         /* All buttons must be polled, like this: */
-        for(int f = 0; f < HARDWARE_NUMBER_OF_FLOORS; f++){
-            if(hardware_read_floor_sensor(f)){
-                hardware_command_floor_indicator_on(f);
+        for(int floor = 0; floor < HARDWARE_NUMBER_OF_FLOORS; floor++){
+            if(hardware_read_floor_sensor(floor)){
+                hardware_command_floor_indicator_on(floor);
             }
         }
 
         /* Lights are set and cleared like this: */
-        for(int f = 0; f < HARDWARE_NUMBER_OF_FLOORS; f++){
+        for(int floor = 0; floor < HARDWARE_NUMBER_OF_FLOORS; floor++){
             /* Internal orders */
-            if(hardware_read_order(f, HARDWARE_ORDER_INSIDE)){
-                hardware_command_order_light(f, HARDWARE_ORDER_INSIDE, 1);
+            if(hardware_read_order(floor, HARDWARE_ORDER_INSIDE)){
+                hardware_command_order_light(floor, HARDWARE_ORDER_INSIDE, 1);
             }
 
             /* Orders going up */
-            if(hardware_read_order(f, HARDWARE_ORDER_UP)){
-                hardware_command_order_light(f, HARDWARE_ORDER_UP, 1);
+            if(hardware_read_order(floor, HARDWARE_ORDER_UP)){
+                hardware_command_order_light(floor, HARDWARE_ORDER_UP, 1);
             }
 
             /* Orders going down */
-            if(hardware_read_order(f, HARDWARE_ORDER_DOWN)){
-                hardware_command_order_light(f, HARDWARE_ORDER_DOWN, 1);
+            if(hardware_read_order(floor, HARDWARE_ORDER_DOWN)){
+                hardware_command_order_light(floor, HARDWARE_ORDER_DOWN, 1);
             }
         }
+}
 
-        /* Code to clear all lights given the obstruction signal */
-        if(hardware_read_obstruction_signal()){
-            hardware_command_stop_light(1);
-            clear_all_order_lights();
-        }
-        else{
-            hardware_command_stop_light(0);
-        }
+
+int main(){
+    if(hardware_init() != 0){
+        fprintf(stderr, "Unable to initialize hardware\n");
+        exit(1);
+    }
+    printf("Press the stop button on the elevator panel to exit\n");
+
+
+    hardware_command_movement(HARDWARE_MOVEMENT_DOWN);
+
+    while(true){
+
+      if(hardware_read_floor_sensor(0))
+        hardware_command_movement(HARDWARE_MOVEMENT_UP);
+
+      if(hardware_read_stop_signal())
+        printf("hehe");
+
+      // if(hardware_read_floor_sensor(HARDWARE_NUMBER_OF_FLOORS-2))
+      //   hardware_command_movement(HARDWARE_MOVEMENT_STOP);
+
+      lights();
     }
 
     return 0;
